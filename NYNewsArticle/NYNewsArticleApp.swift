0db32245d0c0794b82d13fa09183bf21f5a170af
sushobhit jain <sushobhitt.builtbyblank@gmail.com>
2025-08-24 21:40:06 +0530

    Initial Commit

---
+++ NYNewsArticle/NYNewsArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYNewsArticleApp.swift
+//  NYNewsArticle
+//
+//  Created by Sushobhit Jain on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NYNewsArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
